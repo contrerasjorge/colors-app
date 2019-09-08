f6a9cdf4f6135414667d31f8da8dfebfd13f0fc0
Jorge Contreras <jorge.contreras789@gmail.com>
2019-09-08 07:04:11 -0400

    created palette component

---
+++ src/components/Palette.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Palette extends React.Component {
+  render() {
+    return (
+      <div className="Palette">
+        <div className="Palette-colors">placeholder</div>
+      </div>
+    );
+  }
+}
+
+export default Palette;
