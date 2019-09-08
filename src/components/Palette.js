52de1ee610c3456a82c49d292df28c6553859f3c
Jorge Contreras <jorge.contreras789@gmail.com>
2019-09-08 08:51:39 -0400

    add navbar and font

--- src/components/Palette.js
+++ src/components/Palette.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import Slider from 'rc-slider';
-import 'rc-slider/assets/index.css';
-import ColorBox from './ColorBox';
 
+import ColorBox from './ColorBox';
+import Navbar from './Navbar';
 import './Palette.css';
 
 class Palette extends Component {
@@ -23,15 +22,7 @@ class Palette extends Component {
     ));
     return (
       <div className="Palette">
-        <div className="slider">
-          <Slider
-            defaultValue={level}
-            min={100}
-            max={900}
-            onAfterChange={this.changeLevel}
-            step={100}
-          />
-        </div>
+        <Navbar level={level} changeLevel={this.changeLevel} />
         <div className="Palette-colors">{colorBoxes}</div>
       </div>
     );
